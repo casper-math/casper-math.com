2e80edde0b2be904abad58665f520714eee5b5c0
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-15 14:40:36 +0200

    Install Svelte

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
+import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
 import { defineConfig } from 'astro/config'
 
 export default defineConfig({
-    integrations: [tailwind()]
+    integrations: [tailwind(), svelte()]
 })
