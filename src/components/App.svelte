d179fc8acc4f01ced8108b098b27047854b5edfc
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-15 14:53:59 +0200

    Create a Svelte component

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,12 @@
 <script lang="ts">
-	let name: string = 'world';
+let input: string = ''
 </script>
 
-<h1>Hello {name}!</h1>
+<input
+	bind:value={input}
+	type="text"
+	autofocus
+	placeholder="Type an expression..."
+	class="w-full px-6 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:ring-1 focus:ring-blue-600 focus:border-blue-600 focus:outline-none"
+	class:font-mono={input !== ''}
+/>
