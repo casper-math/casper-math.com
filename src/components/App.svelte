9975e70c65b80427535ecc0acb9f431346773e61
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-15 16:43:55 +0200

    Use the `casper-math` package

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,5 +1,9 @@
-<script lang="ts">
-let input: string = ''
+<script>
+import casper from 'casper-math'
+
+let input = '2 + 3 + 7'
+
+$: output = casper().go(input)
 </script>
 
 <input
@@ -10,3 +14,5 @@ let input: string = ''
 	class="w-full px-6 py-4 text-lg border border-gray-300 rounded-md shadow-sm focus:ring-1 focus:ring-blue-600 focus:border-blue-600 focus:outline-none"
 	class:font-mono={input !== ''}
 />
+
+<div>{output}</div>
