1395b5834be1bd795b634cd77d1cc5bbaf3d5b2a
Jeroen van Rensen <jmjrensen@jcjcj.nl>
2023-09-19 20:37:14 +0200

    It shows a preview of the input

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -2,29 +2,47 @@
     import casper from 'casper-math'
     import katex from 'katex'
 
-    let input: string = '2 * 3 + 4 * 5'
-    let output: string = ''
-    $: latex = katex.renderToString(input, { throwOnError: false })
+    let input: string = '(2 + 3) / (4 * 5)'
+    let preview: string = ''
 
-    function go() {
-        let result = casper().go(input)
-        output = katex.renderToString(result, { throwOnError: false })
-    }
+    $: try {
+        let result = casper().actions([]).options({ output: 'preview' }).go(input)
+        preview = katex.renderToString(result, { throwOnError: false, displayMode: true })
+    } catch (error) {}
 </script>
 
-<div class="flex">
+<div class="max-w-screen-lg mx-[calc((100vw-1024px)/2)] w-full flex items-center absolute -translate-y-1/2">
+    <!-- svelte-ignore a11y-autofocus -->
     <input
         bind:value={input}
         type="text"
         autofocus
         placeholder="Type an expression..."
-        class="w-full px-6 py-4 text-lg border-2 border-r-0 border-gray-200 shadow-sm rounded-l-md focus:border-blue-600 focus:outline-none"
+        class="px-8 py-6 text-lg w-full focus:outline-none rounded-l-md shadow-lg border border-gray-200"
         class:font-mono={input !== ''}
+        class:tracking-wide={input === ''}
     />
 
+    <button class="absolute right-[140px] text-gray-600 hover:text-gray-900 hover:rotate-45 transition duration-300">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="w-6 h-6"
+        >
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"
+            />
+            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+        </svg>
+    </button>
+
     <button
-        on:click={go}
-        class="flex items-center px-6 py-4 space-x-1 text-lg text-white transition bg-blue-600 group focus:outline-none focus:bg-blue-700 hover:bg-blue-700 rounded-r-md"
+        class="flex items-center p-6 space-x-1 text-lg shadow-lg bg-gray-200 hover:bg-gray-300 hover:border-gray-300 focus:border-gray-300 transition focus:outline-none focus:bg-gray-300 focus:shadow-lg rounded-r-md border border-gray-200 group"
     >
         <span>Go!</span>
 
@@ -41,5 +59,11 @@
     </button>
 </div>
 
-<div class="flex items-center p-6 my-4 bg-gray-200"><div class="w-20 font-bold">Input:</div> {@html latex}</div>
-<div class="flex items-center p-6 my-4 bg-gray-200"><div class="w-20 font-bold">Output:</div> {@html output}</div>
+<div class="bg-gradient-to-b pt-16 from-gray-200 to-white min-h-[300px]">
+    {#if preview !== '' && input !== ''}
+        <div class="flex max-w-screen-lg mx-auto items-center space-x-3">
+            <p class="text-lg"><strong>Your Input:</strong></p>
+            <div class="text-xl">{@html preview}</div>
+        </div>
+    {/if}
+</div>
